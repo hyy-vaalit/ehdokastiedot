53de1500c7103db01d95d2d46db316814f0ef6dd
Sami Saada <sami.saada@enemy.fi>
2011-07-14 16:35:23 +0300

    added cover_me and missing tests for advocate

--- Gemfile
+++ Gemfile
@@ -15,4 +15,5 @@ group :development, :test do
   gem 'database_cleaner'
   gem 'rspec'
   gem 'rspec-rails'
+  gem 'cover_me'
 end
--- spec/models/advocate_spec.rb
+++ spec/models/advocate_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe Advocate do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :each do
+    @advocate = Advocate.new
+  end
+
+  describe "are able to be nominated" do
+
+    it "should be nominated if person is nominated to be primary advocate" do
+      @advocate.primary_nominated = ElectoralAlliance.new
+      @advocate.nominated.should_not be_nil
+    end
+
+    it "should be nominated if person is nominated to be secondary advocate" do
+      @advocate.secondary_nominated = ElectoralAlliance.new
+      @advocate.nominated.should_not be_nil
+    end
+
+    it "shouldn't be nominated if person is nominated to be neither primary or secondary advocate" do
+      @advocate.nominated.should be_nil
+    end
+
+  end
+
+  it "should have formatted name" do
+    lastname = "Doe"
+    firstname = "Jane"
+    @advocate.lastname = lastname
+    @advocate.firstname = firstname
+    @advocate.name.should == "#{lastname} #{firstname}"
+  end
+
 end
