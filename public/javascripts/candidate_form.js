f26f8ec46c93351223ec16b91da9d3995e1ad421
Sami Saada <sami.saada@enemy.fi>
2011-07-15 17:33:25 +0300

    added candidate email guessing

--- public/javascripts/candidate_form.js
+++ public/javascripts/candidate_form.js
@@ -6,9 +6,17 @@ $(document).ready(function(){
       var lastname = $('#candidate_lastname').val();
       var firstname = $('#candidate_firstname').val();
       var candidate_name_input = $(this);
-      var value = candidate_name_input.val();
-      if (value == "") {
+      if (!candidate_name_input.val()) {
         candidate_name_input.val(lastname + ', ' + firstname);
+      }
+    });
+
+    $('#candidate_email').bind('focusin', function(event) {
+      var lastname = $('#candidate_lastname').val().toLowerCase();
+      var firstname = $('#candidate_firstname').val().toLowerCase();
+      var candidate_email_input = $(this);
+      if (!candidate_email_input.val()) {
+        candidate_email_input.val(firstname + '.' + lastname + '@helsinki.fi');
       }
     });
 
