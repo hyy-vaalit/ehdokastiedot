96ac9e08987e18f0e2b14e90dfaac4d69b046137
Sami Saada <sami.saada@enemy.fi>
2011-07-29 14:20:50 +0300

    added spec structure

---
+++ lib/proportionals.rb
@@ -0,0 +1,2 @@
+module Proportionals
+end
---
+++ spec/lib/proportionals_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Proportionals do
+
+  it "should do calculus for electoral alliances"
+
+  it "should do calculus for electoral coalitions"
+
+end
