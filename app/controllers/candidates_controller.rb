588013bed9f7d76f009a3939729de53fa211c2b8
Sami Saada <sami.saada@enemy.fi>
2011-08-12 10:23:24 +0300

    authentication and authorization for candidate fixes

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,15 +2,26 @@ class CandidatesController < ApplicationController
 
   skip_authorization_check
 
+  before_filter :authenticate_advocate, :authorize_for_candidate
+
   def show
-    @candidate = Candidate.find_by_id params[:id]
   end
 
   def report_fixes
-    candidate = Candidate.find_by_id(params[:id])
-    df = candidate.data_fixes.create! :field_name => params[:field], :old_value => candidate.send(params[:field]), :new_value => params[:new_value]
+    df = @candidate.data_fixes.create! :field_name => params[:field], :old_value => candidate.send(params[:field]), :new_value => params[:new_value]
     render :json => df
   end
 
+  private
+
+  def authenticate_advocate
+    @advocate = AdvocateUser.find_by_id(session[:advocate])
+    redirect_to new_advocate_session_path unless @advocate
+  end
+
+  def authorize_for_candidate
+    @candidate = Candidate.find_by_id(params[:id])
+    redirect_to advocate_path unless @candidate.electoral_alliance.advocate_user == @advocate
+  end
 
 end
