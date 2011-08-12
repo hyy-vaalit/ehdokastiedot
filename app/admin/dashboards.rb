bc3584fb525c0efaaecfd33ec47e5809393b6b20
Sami Saada <sami.saada@enemy.fi>
2011-08-12 15:59:39 +0300

    added showdown link to dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -14,6 +14,7 @@ ActiveAdmin::Dashboards.build do
       li link_to 'Simple list', simple_listings_path
       li link_to 'Same social security number list', same_ssn_listings_path
       li link_to 'Proportional order list', proportional_order_listings_path
+      li link_to 'Showdown, place to control the show', showdown_listings_path
     end
   end
 
