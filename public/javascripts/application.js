83391280d285b194faf934da138aaf26bf3bd692
Sami Saada <sami.saada@enemy.fi>
2011-08-12 17:40:12 +0300

    disabled submit buttons on form submit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function () {
+
+  $('form').bind('submit', function (event) {
+    $(this).find('input:submit').attr('disabled','disabled');
+  });
+
+});
