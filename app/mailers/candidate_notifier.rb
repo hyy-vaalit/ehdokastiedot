cb8b832842802cba6e9aa04b4592fd4726c83a52
Sami Saada <sami.saada@enemy.fi>
2011-08-19 13:55:10 +0300

    fixed bcc array problem

--- app/mailers/candidate_notifier.rb
+++ app/mailers/candidate_notifier.rb
@@ -2,7 +2,7 @@ class CandidateNotifier < HyyMailer
 
   def welcome_as_candidate(addresses, email)
     @email = email
-    mail(:bcc => addresses, :subject => @email.subject)
+    mail(:bcc => addresses.join(','), :subject => @email.subject)
   end
 
 end
