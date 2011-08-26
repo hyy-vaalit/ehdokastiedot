c13a3e2c8cb33124a918954023b0e6d6940e5cfd
Sami Saada <sami.saada@enemy.fi>
2011-08-26 16:01:20 +0300

    added javascript drawing

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -13,6 +13,8 @@ $(document).ready(function () {
   $('a[href="#accept"]').live('click', acceptFix);
   $('a[href="#reject"]').live('click', rejectFix);
 
+  $('a[href="#machinedraw"]').live('click', machinedraw);
+
 });
 
 function acceptFix(event) {
@@ -30,5 +32,28 @@ function actFix(method, element) {
   id = (row.attr('id')).replace('fix_','');
   $.post(window.location.href, {'method': method, 'fix_id': id}, function(data) {
     row.remove();
+  });
+}
+
+function machinedraw(event) {
+  event.preventDefault();
+
+  //+ Jonas Raoni Soares Silva
+  //@ http://jsfromhell.com/array/shuffle [v1.0]
+  shuffle = function(o){ //v1.0
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  };
+
+  text_fields = $(this).parent().parent().find('input[type="text"]');
+  count = text_fields.length;
+  values = [];
+  for(var i=1; i <= count; i++) {
+    values.push(i)
+  }
+  values = shuffle(values)
+  $.each(text_fields, function(i, field) {
+    value = values.pop();
+    $(field).val(value);
   });
 }
