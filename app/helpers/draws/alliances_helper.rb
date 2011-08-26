2c9b349af1143b855ea9220917c095f62ef2290a
Sami Saada <sami.saada@enemy.fi>
2011-08-26 15:10:58 +0300

    added candidate number to be shown in alliance draws

--- app/helpers/draws/alliances_helper.rb
+++ app/helpers/draws/alliances_helper.rb
@@ -1,2 +1,7 @@
 module Draws::AlliancesHelper
+
+  def get_draw_candidate_from_alliance drawing
+    drawing.electoral_alliance.candidates.selection_order[drawing.position_in_alliance].candidate_number
+  end
+
 end
