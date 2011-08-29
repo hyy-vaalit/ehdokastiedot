74de0b1802f35f75557ff9035d0963fd1b4ca3d1
Petrus Repo <petrus.repo@iki.fi>
2011-08-29 13:58:19 +0300

    namespaced seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
 # coding: utf-8
 
-# Configurations
-REDIS.set 'mailaddress', 'hostmaster-hyy@enemy.fi'
-
-# Create a default user
-AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password', :role => 'admin')
+puts "See: rake -T seed"
