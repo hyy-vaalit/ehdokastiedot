b93e45dcc1a870d2b79e1c471eb36047655a0e05
Petrus Repo <petrus.repo@iki.fi>
2011-09-13 15:30:15 +0300

    fix #bug 17895513

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -43,7 +43,8 @@ class Candidate < ActiveRecord::Base
 
   scope :without_electoral_alliance, joins(:electoral_alliance).where('candidates.candidate_name = electoral_alliances.name')
 
-  scope :has_fixes, lambda { joins(:data_fixes) & DataFix.unapplied }
+  # Operator '&' is an intersection, ie, it matches only entities which are present in both groups.
+  scope :has_fixes, lambda { joins(:data_fixes).select("distinct candidates.id, candidates.*") & DataFix.unapplied }
 
   scope :selection_order, order('coalition_proportional desc, alliance_proportional desc')
   scope :by_alliance, order('electoral_alliance_id desc')
