dab71fdc1b9ecd8e623eb13f5580e7601ffc7fd0
Petrus Repo <petrus.repo@iki.fi>
2011-10-05 18:10:38 +0300

    find duplicate alliance results

--- app/models/candidate_result.rb
+++ app/models/candidate_result.rb
@@ -9,4 +9,11 @@ class CandidateResult < ActiveRecord::Base
   def self.elect!(candidate_ids, result_id)
     update_all ["elected = ?", true], ["result_id = ? AND candidate_id IN (?)", result_id, candidate_ids]
   end
+
+  def self.find_duplicate_vote_sums(result_id)
+    select('candidates.electoral_alliance_id, candidate_results.vote_sum_cache').from('candidate_results').joins(
+      'inner join candidates on candidate_results.candidate_id = candidates.id').where(
+      'candidate_results.result_id = ?', result_id).group(
+      'candidates.electoral_alliance_id, candidate_results.vote_sum_cache having count(*) > 1').order('electoral_alliance_id')
+  end
 end
