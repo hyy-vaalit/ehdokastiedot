758b4731c8d24d6d4d74e2cad5897a3d4ebcf33b
Petrus Repo <petrus.repo@iki.fi>
2012-06-27 11:09:47 +0300

    Electoral Alliance can be modified by advocate

--- app/controllers/advocates/alliances_controller.rb
+++ app/controllers/advocates/alliances_controller.rb
@@ -1,16 +1,31 @@
 # encoding: utf-8
 class Advocates::AlliancesController < AdvocatesController
 
+  before_filter :find_alliance, :only => [:show, :edit, :update]
+
   def index
     @alliances = current_advocate_user.electoral_alliances
   end
 
-  def show
-    @alliance = current_advocate_user.electoral_alliances.find(params[:id])
-  end
-
   def new
     @alliance = current_advocate_user.electoral_alliances.build
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @alliance.update_attributes(params[:electoral_alliance])
+      flash[:notice] = "Muutokset tallennettu."
+    else
+      flash[:alert] = "Muutosten tallentaminen epäonnistui!"
+      render :action => :edit and return
+    end
+
+    redirect_to advocates_alliance_path(@alliance)
   end
 
   def create
@@ -18,10 +33,17 @@ class Advocates::AlliancesController < AdvocatesController
 
     if @alliance.save
       flash[:notice] = "Vaaliliitto luotu!"
-      redirect_to advocates_alliance_path(@alliance)
     else
       flash[:alert] = "Vaaliliiton luominen epäonnistui."
-      render :action => :new
+      render :action => :new and return
     end
+
+    redirect_to advocates_alliance_path(@alliance)
+  end
+
+  protected
+
+  def find_alliance
+    @alliance = current_advocate_user.electoral_alliances.find(params[:id])
   end
 end
