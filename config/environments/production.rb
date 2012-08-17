6e5d5a3fe2cff6641a07fd021484a89ae2d0f3b4
Petrus Repo <petrus.repo@iki.fi>
2012-08-17 16:03:28 +0300

    Add sendgrid configuration - no longer works without it in Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,4 +49,12 @@ Vaalit::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'vaalit.hyy.fi' }
 
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 end
