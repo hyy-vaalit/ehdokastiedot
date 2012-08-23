57c4cb576209a347a04b4621a309b134e93a903d
Petrus Repo <petrus.repo@iki.fi>
2012-08-23 17:01:11 +0300

    Deploy documentation

--- config/initializers/000_vaalit.rb
+++ config/initializers/000_vaalit.rb
@@ -7,7 +7,7 @@ module Vaalit
   end
 
   module Results
-    S3_BUCKET_NAME  = ENV['S3_BUCKET_NAME'] || "hyy-vaalit-staging"
+    S3_BUCKET_NAME  = ENV['S3_BUCKET_NAME'] || "hyy-vaalitulos-staging"
     S3_BASE_URL     = ENV['S3_BASE_URL'] || "s3.amazonaws.com"
     PUBLIC_FILENAME = "tulos-alustava.txt"
     S3_BUCKET_URL   = "http://#{S3_BASE_URL}/#{S3_BUCKET_NAME}"
--- config/initializers/amazon_s3_storage.rb
+++ config/initializers/amazon_s3_storage.rb
@@ -1,3 +1,6 @@
+# See also:
+#   config/initializers/000_vaalit.rb
+
 if Rails.env.production?
   AWS::S3::Base.establish_connection!(
     :access_key_id     => ENV['S3_ACCESS_KEY_ID'],
