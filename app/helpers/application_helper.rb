d50cdb63014ca610bdc9470d5a4307191dc5590a
Petrus Repo <petrus.repo@iki.fi>
2012-10-30 18:06:56 +0200

    Treemap from an arbitrary json data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
     alliance.candidates.count == alliance.expected_candidate_count
   end
 
+  def chart_address(result, chart_type)
+    base_url = Vaalit::Results::PUBLIC_RESULT_URL
+
+    "#{base_url}/#{chart_type}.html?json=#{result.filename('.json')}"
+  end
 end
--- config/initializers/000_vaalit.rb
+++ config/initializers/000_vaalit.rb
@@ -17,7 +17,7 @@ module Vaalit
     S3_BUCKET_NAME  = ENV['S3_BUCKET_NAME'] || "hyy-koe"
     S3_BASE_URL     = ENV['S3_BASE_URL'] || "s3.amazonaws.com"
 
-    RESULT_ADDRESS  = ENV['RESULT_ADDRESS'] || "http://vaalitulos.hyy.fi"
+    RESULT_ADDRESS  = ENV['RESULT_ADDRESS'] || "http://hyy-koe.s3-website-us-east-1.amazonaws.com"
     DIRECTORY       = Time.now.year
     PUBLIC_RESULT_URL = "#{RESULT_ADDRESS}/#{DIRECTORY}"
 
