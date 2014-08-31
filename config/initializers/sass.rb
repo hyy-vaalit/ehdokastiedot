43c485488cc48870232f7b8ba5c0cfaeffa3ef6a
Petrus Repo <petrus.repo@iki.fi>
2014-08-31 16:00:20 +0300

    Changes for Asset Pipeline

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,11 +1,17 @@
-# Comment out this line temporarily if you want
-# to regenerate active_admin.css in public/stylesheets
-Sass::Plugin.options.merge!(
-  :template_location => 'public/stylesheets/sass',
-  :css_location => 'tmp/stylesheets'
-)
 
-Rails.configuration.middleware.delete('Sass::Plugin::Rack')
-Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
+############## TODO TODO TODO
+## Ensure settings in this file are adapted to Rails 3.1 structure.
+## Then remove this file.
 
-Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static', :urls => ['/stylesheets'], :root => "#{Rails.root}/tmp")
+
+# # Comment out this line temporarily if you want
+# # to regenerate active_admin.css in public/stylesheets
+# Sass::Plugin.options.merge!(
+#   :template_location => 'public/stylesheets/sass',
+#   :css_location => 'tmp/stylesheets'
+# )
+#
+# Rails.configuration.middleware.delete('Sass::Plugin::Rack')
+# Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
+#
+# Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static', :urls => ['/stylesheets'], :root => "#{Rails.root}/tmp")
