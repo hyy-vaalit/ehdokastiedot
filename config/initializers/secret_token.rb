e51745e21ed6d619733028df71ad9254c43b9faa
Petrus Repo <petrus.repo@iki.fi>
2014-08-31 11:45:27 +0300

    Updated secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Vaalit::Application.config.secret_token = 'bee0553bba6f27d9202a0900bd9cd23f869e42a8a918a0a545f5b31f38c4d571f8e24962b5051ec2cf0dc151275e50dce60e2fc57350ad102617bb802485aaf1'
+Vaalit::Application.config.secret_token = 'bee0553bba6f27d9202a0900bd9cd23f869e42a8a918a0a545f5b31f38c4d571f8mdu37dlposxcvmko1ec2cf0dc151275e50dce60e2fc57350ad102617bb802485aaf1'
