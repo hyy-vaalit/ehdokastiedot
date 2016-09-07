e54d34edd40b89fde610d040987a216718e2463a
Petrus Repo <petrus.repo@iki.fi>
2016-09-07 13:03:43 +0300

    refact (env) Read ENV or raise exception

--- config/initializers/000_vaalit.rb
+++ config/initializers/000_vaalit.rb
@@ -1,11 +1,11 @@
 module Vaalit
 
   module Public
-    SITE_ADDRESS        = "https://vaalit.hyy.fi"
+    SITE_ADDRESS        = ENV.fetch 'SITE_ADDRESS'
     ADVOCATE_LOGIN_URL  = "#{SITE_ADDRESS}"
     SECRETARY_LOGIN_URL = "#{SITE_ADDRESS}/admin"
-    EMAIL_FROM_ADDRESS  = "vaalit@hyy.fi"
-    EMAIL_FROM_NAME     = "Silva Loikkanen"
+    EMAIL_FROM_ADDRESS  = ENV.fetch 'EMAIL_FROM_ADDRESS'
+    EMAIL_FROM_NAME     = ENV.fetch 'EMAIL_FROM_NAME'
   end
 
   module Config
@@ -17,23 +17,16 @@ module Vaalit
   module Voting
     PROPORTIONAL_PRECISION = 5   # Decimals used in proportional numbers (eg. 87.12345 is 5 decimals)
     ELECTED_CANDIDATE_COUNT = 60 # How many candidates are elected
-
-    if (ENV['VOTING_CALCULATION_BEGINS_AT'])
-      CALCULATION_BEGINS_AT = Time.parse(ENV['VOTING_CALCULATION_BEGINS_AT'])
-    else
-      raise "Set ENV['VOTING_CALCULATION_BEGINS_AT'] in .env"
-    end
-
+    CALCULATION_BEGINS_AT   = Time.parse ENV.fetch('VOTING_CALCULATION_BEGINS_AT')
   end
 
   module Results
-    S3_BUCKET_NAME  = ENV['S3_BUCKET_NAME'] || "hyy-koe"
-    S3_BASE_URL     = ENV['S3_BASE_URL'] || "s3.amazonaws.com"
+    S3_BUCKET_NAME  = ENV.fetch 'S3_BUCKET_NAME'
+    S3_BASE_URL     = ENV.fetch 'S3_BASE_URL'
 
-    RESULT_ADDRESS  = ENV['RESULT_ADDRESS'] || "http://hyy-koe.s3-website-us-east-1.amazonaws.com"
+    RESULT_ADDRESS  = ENV.fetch 'RESULT_ADDRESS'
     DIRECTORY       = Time.now.year
     PUBLIC_RESULT_URL = "#{RESULT_ADDRESS}/#{DIRECTORY}"
-
   end
 
   module AWS
@@ -43,8 +36,8 @@ module Vaalit
 
     if connect?
       ::AWS::S3::Base.establish_connection!(
-        :access_key_id     => ENV['S3_ACCESS_KEY_ID'],
-        :secret_access_key => ENV['S3_ACCESS_KEY_SECRET']
+        :access_key_id     => ENV.fetch('S3_ACCESS_KEY_ID'),
+        :secret_access_key => ENV.fetch('S3_ACCESS_KEY_SECRET')
       )
     end
   end
