2cb6233d606d604b919a70c07ccb7bce25cc4140
Petrus Repo <petrus.repo@iki.fi>
2022-06-18 15:34:45 +0300

    Define AdminUser roles as enum

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -17,7 +17,7 @@ ActiveAdmin.register AdminUser do
   form do |f|
     f.inputs "Admin Details" do
       f.input :email
-      f.input :role, :as => :select, :collection => AdminUser::ROLES
+      f.input :role, :as => :select, :collection => AdminUser.roles.keys
     end
     f.actions
   end
