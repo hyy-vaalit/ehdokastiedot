5f09d49f997f3d813b168245b6ce9ae5164e18f2
Petrus Repo <petrus.repo@iki.fi>
2022-06-18 17:00:44 +0300

    Trim AdvocateUser attributes

---
+++ db/migrate/20220618134907_trim_advocate_user_attrs.rb
@@ -0,0 +1,11 @@
+class TrimAdvocateUserAttrs < ActiveRecord::Migration[6.1]
+  def change
+    change_table :advocate_users do |t|
+      t.remove :ssn
+      t.string :student_number, null: false, default: "bogus"
+      t.remove :postal_address
+      t.remove :postal_code
+      t.remove :postal_city
+    end
+  end
+end
