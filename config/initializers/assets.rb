9e7f4750ff514add152783434114643c5e4be0d9
Petrus Repo <petrus.repo@iki.fi>
2022-07-27 20:06:33 +0300

    Remove Internet Explorer 6 and 7 related compatibility assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( formtastic_ie6.css formtastic_ie7.css )
+# Rails.application.config.assets.precompile += %w( formtastic_ie6.css formtastic_ie7.css )
