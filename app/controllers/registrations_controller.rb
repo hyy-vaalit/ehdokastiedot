3296b7128bd2aa755bef225b66c4caecd65c5f57
Petrus Repo <petrus.repo@iki.fi>
2022-07-29 13:41:17 +0300

    Require HakaUser on all /registrarion paths

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,18 @@
 class RegistrationsController < ApplicationController
   skip_authorization_check only: :index
 
+  before_action :require_haka_user
+
   def index
     @candidate = Candidate.valid.find_by(student_number: current_haka_user.student_number)
+  end
+
+  protected
+
+  def require_haka_user
+    if current_haka_user.nil?
+      flash.now.alert = "Kirjaudu ensin sisään."
+      render "common/unauthorized", status: 401 and return
+    end
   end
 end
