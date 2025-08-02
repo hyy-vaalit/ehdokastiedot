74f9c98a818469c5b99232ff41cd9004e4c2fbaf
Petrus Repo <petrus.repo@iki.fi>
2025-08-02 18:20:57 +0300

    Fix Active Support deprecation on enum

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,10 +1,7 @@
 class AdminUser < ActiveRecord::Base
   include DeviseUserBehaviour
 
-  enum role: {
-    "admin" => "admin",
-    "secretary" => "secretary"
-  }
+  enum(role: { "admin" => "admin", "secretary" => "secretary" })
 
   # Devise modules
   # https://www.rubydoc.info/github/plataformatec/devise/Devise/Models
