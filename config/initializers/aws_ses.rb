793d6cea117f8614a5145c934c09ca7be1d54670
Petrus Repo <petrus.repo@iki.fi>
2025-10-04 18:06:22 +0300

    Declare :aws_ses delivery method

--- config/initializers/aws_ses.rb
+++ config/initializers/aws_ses.rb
@@ -1,8 +1,8 @@
 Aws::Rails.add_action_mailer_delivery_method(
-  :aws_sdk,
+  :aws_ses,
   credentials: Aws::Credentials.new(
     ENV.fetch('AWS_ACCESS_KEY_ID'),
     ENV.fetch('AWS_SECRET_ACCESS_KEY'),
   ),
-  region: ENV.fetch('AWS_REGION', 'eu-central-1')
+  region: ENV.fetch('AWS_REGION', 'eu-west-1')
 )
